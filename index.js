ed3fc81668ffe6fd6ca7e52256953603bec937f9
dantvi <daniel.tving@gmail.com>
2024-09-30 14:26:07 +0200

    finding habitable planets

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const { parse } = require('csv-parse');
 const fs = require('fs');
 
-const results = [];
+const habitablePlanets = [];
+
+// Checks if the planet is confirmed as well as light conditions and planet size
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
+}
 
 // Reading raw data from the file and parsing the data to an array with objects
 fs.createReadStream('kepler_data.csv')
@@ -10,13 +17,14 @@ fs.createReadStream('kepler_data.csv')
         columns: true,
     }))
     .on('data', (data) => {
-        results.push(data);
+        if (isHabitablePlanet(data)) {
+            habitablePlanets.push(data);
+        }
     })
     .on('error', (err) => {
         console.log(err);
     })
     .on('end', () => {
-        console.log('resultsArray:', results);
-        console.log('done');
+        console.log(`${habitablePlanets.length} habitable planets found`);
     });
 
